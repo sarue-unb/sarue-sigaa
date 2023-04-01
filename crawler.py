c28aaad47a477c6e39619d8811e7f0125f6f87c3
João Pedro Sadéri da Silva <jpsaderi@hotmail.com>
2023-04-01 09:39:00 -0300

    initial commit of the new version of the plugin

--- crawler.py
+++ crawler.py
@@ -1,16 +1,15 @@
-from selenium import webdriver
 import time
-import components.selection_components as dc
-import pages.sigaa_pages as sp
 import pages.extension_page as ep
-from dotenv import dotenv_values
+import pages.sigaa_pages as sp 
+import components.selection_components as dc
+from selenium import webdriver # pip install selenium
+from dotenv import dotenv_values # pip install python-dotenv
 
 
 class Crawler:
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.env = dotenv_values(".env")
-        
     
     def run(self):
         print("Running Crawler")
@@ -24,4 +23,3 @@ class Crawler:
         time.sleep(5)
         self.driver.quit()
         print("Ending Crawler")
-        
