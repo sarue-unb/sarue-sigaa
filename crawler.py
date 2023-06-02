8a27526c867a4b54b110e4db74e016287824cf16
João Pedro Sadéri da Silva <jpsaderi@hotmail.com>
2023-06-02 15:05:48 -0300

    feat: usar multiplos navegadores

--- crawler.py
+++ crawler.py
@@ -9,25 +9,57 @@ from output_format import *
 
 class Crawler:
     def __init__(self):
-        # Aqui pode ser selecionado pelo usuário qual navegador ele que usar.
-        # Importante: Usuarios MacOS normalmente só possuem Safari.
-        # ou até mesmo verificar se o driver retorna algo.
+        try:
+            self.driver = webdriver.Chrome()
+            navegador = "chrome"
+        except Exception as e:
+            try: 
+                self.driver = webdriver.Safari()
+                navegador = "safari"    
+            except Exception as e:
+                try: 
+                    self.driver = webdriver.Edge()
+                    navegador = "edge"
+                except Exception as e:
+                    try: 
+                        self.driver = webdriver.Firefox()
+                        navegador = "firefox"
+                    except Exception as e:
+                        print("Nenhum navegador padrão encontrado.")
+                        navegador = None
+                        exit(1)
 
-        self.driver = webdriver.Chrome()
-        # self.driver = webdriver.Firefox() 
-        # self.driver = webdriver.Edge()
-        # self.driver = webdriver.Safari()
-        
         self.env = dotenv_values(".env")
+
+        print("NAVEGADOR USADO:" + RIGHT_ARROW + navegador.capitalize() + LEFT_ARROW)                
     
     def run(self):
         print(HASH + "Running Crawler" + HASH)
 
         self.driver.get("https://sigaa.unb.br/sigaa/")
       
-        input("Ao chegar na pagina de extensao aperte enter")
+        ### LOGIN     
+        self.driver.implicitly_wait(20) # Espera 20 segundos para o usuário se autenticar.
 
-        dg.get_every_extension_activity_from_years(2023,2023,self.driver)
+        ### Aqui, posso ver qual o perfil do usuário
+        while (self.driver.current_url not in pages_valid['discente'] and self.driver.current_url not in pages_valid['docente']):
+            print(RIGHT_ARROW + "Not logged in yet" + LEFT_ARROW) 
+            time.sleep(5) # Usado para não poluir o console com mensagens de "Not logged in yet"
+        
+        if self.driver.current_url in pages_valid['discente']:
+            print(RIGHT_ARROW + "Logged in as discente"  + LEFT_ARROW)
+            perfil = "discente"
+            ### Navegar para a página de extensão
+            sp.go_into_extension_page(self.driver, self.env)
+            ### 
+        elif self.driver.current_url in pages_valid['docente']: 
+            print(RIGHT_ARROW + "Logged in as docente" + LEFT_ARROW)
+            perfil = "docente"
+        ###
+
+        # input("Ao chegar na pagina de extensao aperte enter")
+        
+        dg.get_every_extension_activity_from_years(2020,2023,self.driver)
         
         time.sleep(20)
         # self.driver.quit()
