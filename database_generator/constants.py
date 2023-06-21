552f4bd0c895a7b55db8d8a7ec42ff30593bc60e
jpsaderi <jpsaderi@hotmail.com>
2023-06-21 17:41:45 -0300

    feat: obter dados pela visualização

--- database_generator/constants.py
+++ database_generator/constants.py
@@ -27,3 +27,6 @@ ROWS_DATA_CELLS = {
 
 PRINTER_FORM_ID_PRE_FIX = 'form:j_id_jsp_1439633680_506:'
 PRINTER_FORM_ID_POS_FIX = ':imprimir'
+
+VIEW_FORM_ID_PRE_FIX = 'form:j_id_jsp_1439633680_506:'
+VIEW_FORM_ID_POS_FIX = ':visualizar'
