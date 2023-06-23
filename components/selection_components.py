4a339af3d3424c3e7a2f2bc8884a8bafd04cb567
jpsaderi <jpsaderi@hotmail.com>
2023-06-22 23:01:35 -0300

    fix: ignorar carga horária em projetos e produtos

--- components/selection_components.py
+++ components/selection_components.py
@@ -91,6 +91,9 @@ def get_info_try_except(xpath, driver):
     except:
         return "N/A"
     
+def get_info_direct(xpath, driver):
+    return get_element_by_xpath(xpath, driver).text
+    
 def get_info_from_print_page(driver):
     info = {}
 
@@ -133,57 +136,59 @@ def get_info_from_print_page(driver):
 def get_info_from_view_page(driver):
     info = {}
 
-    info["codigo"] = get_info_try_except("//html/body/div[2]/div[2]/form/table/tbody/tr[2]/td", driver)
-    info["titulo"] = get_info_try_except("//html/body/div/div[2]/form/table[1]/tbody/tr[3]/td", driver)
-    info["categoria"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[4]/td[1]", driver)
-    info["abrangencia"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[4]/td[2]", driver)
+    info["codigo"] = get_info_direct("//html/body/div[2]/div[2]/form/table/tbody/tr[2]/td", driver)
+    info["titulo"] = get_info_direct("//html/body/div/div[2]/form/table[1]/tbody/tr[3]/td", driver)
 
-    info["ano"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[5]/td[1]", driver)
-    info["periodo_realizacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[5]/td[2]", driver)
-    info["unidade_proponente"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[6]/td", driver)  
-    info["unidade_orcamentaria"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[7]/td", driver)
-    info["executor_financeiro"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[8]/td", driver)
-    info["unidade_coexecutora"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[9]/td", driver)
-    info["outras_unidades_envolvidas"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[10]/td", driver)
-    info["area_cnpq"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[11]/td[1]", driver)
-    info["area_principal"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[11]/td[2]]", driver)
-    info["qtd_bolsas_solicitadas"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[12]/td[1]", driver)
-    info["qtd_bolsas_concedidas"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[12]/td[2]", driver)
-    info["tipo_cadastro"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[13]/td[1]", driver)
-    info["convenio_funpec"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[13]/td[2]", driver)
+    info["categoria"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[4]/td[1]", driver)
+    info["abrangencia"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[4]/td[2]", driver)
+    info["ano"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[5]/td[1]", driver)
+    info["periodo_realizacao"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[5]/td[2]", driver)
+
+    info["unidade_proponente"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[6]/td", driver)  
+    info["unidade_orcamentaria"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[7]/td", driver)
+    info["executor_financeiro"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[8]/td", driver)
+    info["unidade_coexecutora"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[9]/td", driver)
+    info["outras_unidades_envolvidas"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[10]/td", driver)
+
+    info["area_cnpq"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[11]/td[1]", driver)
+    info["area_principal"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[11]/td[2]", driver)
+    info["qtd_bolsas_solicitadas"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[12]/td[1]", driver)
+    info["qtd_bolsas_concedidas"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[12]/td[2]", driver)
+    info["tipo_cadastro"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[13]/td[1]", driver)
+    info["convenio_funpec"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[13]/td[2]", driver)
+    info["publico_alvo_interno"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[14]/td[1]", driver)
+    info["publico_alvo_externo"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[14]/td[2]", driver)
+    info["publico_estimado_externo"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[15]/td[1]", driver)
+    info["publico_estimado_interno"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[15]/td[2]", driver)
+
+    info["publico_real_atingido"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[16]/td", driver)
     
-    info["publico_alvo_interno"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[14]/td[1]", driver)
-    info["publico_alvo_externo"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[14]/td[2]", driver)
-    info["publico_estimado_externo"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[15]/td[1]", driver)
-    info["publico_estimado_interno"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[15]/td[2]", driver)
-    info["publico_real_atingido"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[16]/td", driver)
-    info["fonte_financiamento"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[17]/td[1]", driver)
-    info["renovacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[17]/td[2]", driver)
+    info["fonte_financiamento"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[18]/td[1]", driver)
+    info["renovacao"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[18]/td[2]", driver)
 
-    info["linha_atuacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[18]/td", driver)
-    info["programa_estrategico"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[19]/td", driver)
-    info["vinculado_acao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[20]/td", driver)
-    info["vinculado_grupo"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[21]/td", driver)
-    
-    info["parte_programa_extensao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[22]/td", driver)
-    info["tipo_evento"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[23]/td", driver)
-    info["periodo_evento"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[24]/td", driver)
+    info["linha_atuacao"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[18]/td", driver)
+    info["programa_estrategico"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[19]/td", driver)
+    info["vinculado_acao"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[20]/td", driver)
+    info["vinculado_grupo"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[21]/td", driver)
+    info["parte_programa_extensao"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[22]/td", driver)
+    info["tipo_evento"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[24]/td", driver)
+    info["periodo_evento"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[25]/td", driver)
 
-    info["carga_horaria"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[25]/td[1]", driver)
-    info["previsao_vagas"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[25]/td[2]", driver)
+    if(info["categoria"] == "EVENTO" or info["categoria"] == "CURSO"):
+        info["carga_horaria"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[26]/td[1]", driver)
+        info["previsao_vagas"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[26]/td[2]", driver)
 
-    info["situacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[26]/td", driver)
-    info["reponsavel_acao_nome"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[27]/td", driver)
-    info["reponsavel_acao_email"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[28]/td", driver)
-    info["reponsavel_acao_telefone"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[29]/td", driver)
+    info["situacao"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[27]/td", driver)
+    info["reponsavel_acao_nome"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[28]/td", driver)
+    info["reponsavel_acao_email"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[29]/td", driver)
+    info["reponsavel_acao_telefone"] = get_info_direct("/html/body/div[2]/div[2]/form/table/tbody/tr[30]/td", driver)
 
-    info["estado_realizacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[32]/td/table/tbody/tr[2]/td[1]")
-    info["municipio_realizacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[32]/td/table/tbody/tr[2]/td[2]", driver)
-    info["bairro_realizacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[32]/td/table/tbody/tr[2]/td[3]", driver)
-    info["espaco_realizacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[32]/td/table/tbody/tr[2]/td[4]", driver)
+    # info["estado_realizacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[32]/td/table/tbody/tr[2]/td[1]")
+    # info["municipio_realizacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[32]/td/table/tbody/tr[2]/td[2]", driver)
+    # info["bairro_realizacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[32]/td/table/tbody/tr[2]/td[3]", driver)
+    # info["espaco_realizacao"] = get_info_try_except("/html/body/div[2]/div[2]/form/table/tbody/tr[32]/td/table/tbody/tr[2]/td[4]", driver)
 
     # Aqui precisa ter paciencia, ate fazer uma função para verificar se a imagem contida é light ou não
-
 
     return info
 
