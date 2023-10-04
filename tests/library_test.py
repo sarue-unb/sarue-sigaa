050e2988a820b21dc9fba2f690cde1c5d4604928
jpsaderi <jpsaderi@hotmail.com>
2023-10-04 09:56:25 -0300

    test: library_import

--- tests/library_test.py
+++ tests/library_test.py
@@ -1,29 +1,16 @@
+import importlib
 import unittest
+from config.libraries_descryption import list_libraries
 
 class TestLibrary(unittest.TestCase):
-    def testSelenium(self):
-        try:
-            import selenium
-            has_request = True
-        except ImportError:
-            has_request = False
-        self.assertTrue(has_request, "Selenium is not installed")
-
-    def testDotenv(self):
-        try:
-            from dotenv import dotenv_values
-            has_dotenv = True
-        except ImportError:
-            has_dotenv = False
-        self.assertTrue(has_dotenv, "Dotenv is not installed")
-
-    def testTqdm(self):
-        try:
-            from tqdm import tqdm
-            has_tqdm = True
-        except ImportError:
-            has_tqdm = False
-        self.assertTrue(has_tqdm, "Tqdm is not installed")
+    def testLibraries(self):
+        for library_name, library_import in list_libraries.items():
+            try:
+                importlib.import_module(library_name)
+                has_library = True
+            except ImportError:
+                has_library = False
+            self.assertTrue(has_library, f"{library_import} is not installed")
 
 if __name__ == '__main__':
     unittest.main()
