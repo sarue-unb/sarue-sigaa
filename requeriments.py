5cada71e26f98c951f94d7b569e212cfa1d84046
jpsaderi <jpsaderi@hotmail.com>
2023-10-14 13:06:02 -0300

    feat: force install new versions of requirements

--- requeriments.py
+++ requeriments.py
@@ -1,51 +1,40 @@
-import sys
 import importlib
 import subprocess
-import pkg_resources
+import sys
+
 from config.libraries_descryption import list_libraries
 from config.display_descryption import SEPARATOR, SLEEP_TIME
 from config.crawler_descryption import SCHEDULE
-import time
+
+def update_library(library_name, library_import):
+    try:
+        importlib.import_module(library_name)
+        print(f'{library_name} is installed')
+        print(f'Updating {library_name} to the latest version...')
+        subprocess.check_call([sys.executable, "-m", "pip", "install", library_import, "-U"])
+        print(f'{library_name} has been updated to the latest version.')
+
+    except ImportError:
+        print(f'{library_name} is not installed')
+        print(f'Installing {library_name}...')
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", library_import])
+            print(f'{library_name} has been installed.')
+        except Exception as e:
+            print(f'Error installing {library_name}: {e}')
+            sys.exit(1)
+
+    except Exception as e:
+        print(f'Error updating {library_name}: {e}')
 
 def checkLibraries():
     print(SEPARATOR)
     for library_name, library_import in list_libraries.items():
-        try:
-            importlib.import_module(library_name)
-            installed_version = pkg_resources.get_distribution(library_import).version
-            print(f'{library_name} is installed (Version: {installed_version})')
-            
-            # Check for the latest version on PyPI
-            latest_version = subprocess.check_output([sys.executable, "-m", "pip", "show", library_import]).decode("utf-8")
-            latest_version = [line for line in latest_version.split('\n') if line.startswith('Version:')]
-            if latest_version:
-                latest_version = latest_version[0].split(':', 1)[1].strip()
-                if installed_version != latest_version:
-                    print(f'A newer version ({latest_version}) is available. Updating...')
-                    try:
-                        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", library_import])
-                        print(f'{library_name} has been updated to version {latest_version}.')
-                    except Exception as e:
-                        print(f'Error updating {library_name}: {e}')
-            else:
-                print(f'Could not fetch latest version information for {library_name}')
-        except ImportError as e:
-            print(f'{library_name} is not installed')
-            try:
-                subprocess.check_call([sys.executable, "-m", "pip", "install", library_import])
-                print(f'{library_name} has been installed.')
-            except Exception as e:
-                print(f'Error installing {library_name}: {e}')
-                exit(1)
-        except pkg_resources.DistributionNotFound:
-            print(f'Missing dependencies for {library_name}. Updating...')
-            try:
-                subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", library_import])
-                print(f'{library_name} and its dependencies have been updated.')
-            except Exception as e:
-                print(f'Error updating {library_name} and its dependencies: {e}')
+        update_library(library_name, library_import)
     print(SEPARATOR)
-    if SCHEDULE == True:
+
+    if SCHEDULE:
+        import time
         time.sleep(SLEEP_TIME)
     else:
         input("Press enter to continue...")
